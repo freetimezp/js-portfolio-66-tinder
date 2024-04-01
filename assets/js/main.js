b3944b947af2b3e3ea656586ab0e331522b809fe
freetimezp <kraftukrnet@ukr.net>
2024-04-01 21:49:53 +0300

    04. js-portfolio-66-tinder. GSAP animation.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 //get the data
-var users = [
+let users = [
     {
         profilePic: "https://images.unsplash.com/photo-1583001809898-006ebc1377c1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         displayPic: "https://images.unsplash.com/photo-1567516364473-233c4b6fcfbe?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -142,22 +142,20 @@ function select(elem) {
 }
 
 
-var curr = 0;
+let curr = 0;
+let isAnimating = false;
 
-(function setInitial() {
-    select(".maincard img").src = users[curr].displayPic;
-    select(".incomingcard img").src = users[curr + 1]?.displayPic;
+function setData(index) {
+    select(".prflimg img").src = users[index].profilePic;
 
-    select(".prflimg img").src = users[curr].profilePic;
-
-    select(".badge p").textContent = users[curr].pendingMessage;
-    select(".location h3").textContent = users[curr].location;
-    select(".name").textContent = users[curr].name;
-    select(".age").textContent = users[curr].age;
+    select(".badge p").textContent = users[index].pendingMessage;
+    select(".location h3").textContent = users[index].location;
+    select(".name h1").textContent = users[index].name;
+    select(".age").textContent = users[index].age;
 
 
-    var clutter = "";
-    users[curr].interests.forEach(function (elem) {
+    let clutter = "";
+    users[index].interests.forEach(function (elem) {
         clutter += `<div class="tag flex items-center gap-2 bg-white/30 px-3 py-1 rounded-full">
                     ${elem.icon}
                     <h3 class="text-sm tracking-tighter capitalize">${elem.interest}</h3>
@@ -165,9 +163,117 @@ var curr = 0;
     });
     select(".tags").innerHTML = clutter;
 
-    select(".bio p").innerHTML = users[curr].bio;
+    select(".bio p").innerHTML = users[index].bio;
+}
+
+(function setInitial() {
+    select(".maincard img").src = users[curr].displayPic;
+    select(".incomingcard img").src = users[curr + 1]?.displayPic;
+
+    setData(curr);
 
     curr = 2;
 })();
+
+
+function imageChange() {
+    if (!isAnimating) {
+        isAnimating = true;
+
+        let tl = gsap.timeline({
+            onComplete: function () {
+                isAnimating = false;
+
+                let main = select(".maincard");
+                let incoming = select(".incomingcard");
+
+                incoming.classList.remove("z-[2]");
+                incoming.classList.add("z-[3]");
+                incoming.classList.remove("incomingcard");
+
+                main.classList.remove("z-[3]");
+                main.classList.add("z-[2]");
+                gsap.set(main, {
+                    scale: 1,
+                    opacity: 1
+                });
+
+                if (curr === users.length) curr = 0;
+                select('.maincard img').src = users[curr].displayPic;
+                curr++;
+
+                main.classList.remove("maincard");
+                incoming.classList.add("maincard");
+                main.classList.add("incomingcard");
+
+            }
+        });
+
+        tl.to(".maincard", {
+            scale: 1.1,
+            opacity: 0,
+            ease: Circ,
+            duration: 0.9
+        }, "a");
+        tl.from(".incomingcard", {
+            scale: 0.9,
+            opacity: 0,
+            ease: Circ,
+            duration: 1.1
+        }, "a");
+    }
+}
+
+let deny = select(".deny");
+let accept = select(".accept");
+
+deny.addEventListener('click', function () {
+    imageChange();
+
+    setData(curr - 1);
+
+    gsap.from(".details .element", {
+        y: "100%",
+        opacity: 0,
+        stagger: 0.1,
+        ease: Circ,
+        duration: 1,
+    });
+});
+
+accept.addEventListener('click', function () {
+    imageChange();
+
+    setData(curr - 1);
+
+    gsap.from(".details .element", {
+        y: "100%",
+        opacity: 0,
+        stagger: 0.1,
+        ease: Circ,
+        duration: 1,
+    });
+});
+
+
+(function containerCreator() {
+    document.querySelectorAll(".element").forEach(function (element) {
+        let div = document.createElement("div");
+        div.classList.add(`${element.classList[1]}container`, 'overflow-hidden');
+        //console.log(div);
+
+        div.appendChild(element);
+        select('.details').appendChild(div);
+    });
+})();
+
+gsap.from(".details .element", {
+    y: "100%",
+    opacity: 0,
+    stagger: 0.1,
+    ease: Circ,
+    duration: 1,
+});
+
 
 
